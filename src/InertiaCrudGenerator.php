6c890f18e95ba8995d61251b37b7882326231d76
saidjons <saidjons@bk.ru>
2022-01-02 11:50:10 +0500

    Working on publishing files

--- src/InertiaCrudGenerator.php
+++ src/InertiaCrudGenerator.php
@@ -8,14 +8,7 @@ class InertiaCrudGenerator
   public function boot()
   {
     
-      $this->publishes([
-        __DIR__.'../resources/Components' => resource_path('js/Components'),
-        __DIR__.'../resources/Pages' => resource_path('js/Pages'),
-        __DIR__.'../resources/plugins' => resource_path('js/plugins'),
-    ]);
-
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-
+   
 
   }
 
