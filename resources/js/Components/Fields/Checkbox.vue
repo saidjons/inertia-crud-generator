767a21b77045eb1d06db5862856b9bb92bff650b
saidjons <saidjons@bk.ru>
2022-06-19 12:02:59 +0500

    checkbox label click on check fixed

--- resources/js/Components/Fields/Checkbox.vue
+++ resources/js/Components/Fields/Checkbox.vue
@@ -53,11 +53,12 @@ export default {
     class="form-check">
       <input
          :name="name"
+         :id='name'
       v-model="content"
         @change="inputChanged"
 
-       class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox"   id="flexCheckChecked" :checked='initalValue'>
-      <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">
+       class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox"   id="flexCheckChecked" :checked='initialValue'>
+      <label class="form-check-label inline-block text-gray-800" :for="name">
         {{label}}
       </label>
     </div>
--- resources/js/Components/Sidebar/SidebarItem.vue
+++ resources/js/Components/Sidebar/SidebarItem.vue
@@ -1,61 +1,73 @@
 <script>
-import { Inertia } from '@inertiajs/inertia'
-import { Link } from '@inertiajs/inertia-vue3'
+import { Inertia } from "@inertiajs/inertia";
+import { Link } from "@inertiajs/inertia-vue3";
 
-import {ChevronDownIcon} from '@/Components/Icons/hero';
+import { ChevronDownIcon } from "@/Components/Icons/hero";
 export default {
-  props:['item'],
+    props: ["item"],
 
-  components: {
-ChevronDownIcon,
-Link,
-  },
-  methods: {
-    showSubs(){
-      if(this.item.nested){
-      this.openSubs = !this.openSubs
-      }else{
-        Inertia.visit(this.item.link, { method: 'get' })
-      }
-    }
-  },
-  data() {
-    return{ 
-      openSubs:false,
-    }
-  }
-  
-}
+    components: {
+        ChevronDownIcon,
+        Link,
+    },
+    methods: {
+        showSubs() {
+            if (this.item.nested) {
+                this.openSubs = !this.openSubs;
+            } else {
+                Inertia.visit(this.item.link, { method: "get" });
+            }
+        },
+    },
+    data() {
+        return {
+            openSubs: false,
+        };
+    },
+};
 </script>
 <template>
-     <li class="flex w-full justify-between   cursor-pointer items-center mb-6">
-        <a href="javascript:void(0)" 
-          @click="showSubs"
-         class="flex items-center focus:outline-none focus:ring-2 focus:ring-white w-full">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-grid" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z"></path>
-                <rect x="4" y="4" width="6" height="6" rx="1"></rect>
-                <rect x="14" y="4" width="6" height="6" rx="1"></rect>
-                <rect x="4" y="14" width="6" height="6" rx="1"></rect>
-                <rect x="14" y="14" width="6" height="6" rx="1"></rect>
-            </svg>
-            <span class="text-sm ml-2">{{item.title}}</span>
-        </a>
-        <div v-if="item.badgeNumber" class="py-1 px-3   rounded   flex items-center justify-center text-xs">{{item.badgeNumber}}</div>
-    </li>
-        <div v-show="openSubs" class="bg-white pl-2 mb-2   capitalize   font-semibold">
-            <template v-for="(sub ,subIndex) in item.subs" :key="subIndex" >
-             <li class="flex w-full justify-between   cursor-pointer items-center mb-2 hover:bg-green-200 ">
+    <li class="flex w-full justify-between cursor-pointer items-center mb-6">
+        <a
+            href="javascript:void(0)"
+            @click="showSubs"
+            class="flex items-center focus:outline-none focus:ring-2 focus:ring-white w-full"
+        >
+            <!-- icon here -->
+            <ChevronDownIcon />
 
-              <!-- <a  :href="sub.link" class="  my-1 cursor-pointer items-center w-full  ">
+            <span class="text-sm ml-2">{{ item.title }}</span>
+        </a>
+        <div
+            v-if="item.badgeNumber"
+            class="py-1 px-3 rounded flex items-center justify-center text-xs"
+        >
+            {{ item.badgeNumber }}
+        </div>
+    </li>
+    <div v-show="openSubs" class="bg-white pl-2 mb-2 capitalize font-semibold">
+        <template v-for="(sub, subIndex) in item.subs" :key="subIndex">
+            <li
+                class="flex w-full justify-between cursor-pointer items-center mb-2 hover:bg-green-200"
+            >
+                <!-- <a  :href="sub.link" class="  my-1 cursor-pointer items-center w-full  ">
                 {{sub.title}}
               </a> -->
-              <Link :href="sub.link" 
-               class="  my-1 cursor-pointer items-center w-full  "
-               method="get" as="button" type="button">{{sub.title}}</Link>
-              <div v-if="item.badgeNumber" class="py-1 px-3   rounded   flex items-center justify-center text-xs">{{sub.badgeNumber}}</div>
-             </li>
-            </template >
-        </div>
-                                
+                <Link
+                    :href="sub.link"
+                    class="my-1 cursor-pointer items-center w-full"
+                    method="get"
+                    as="button"
+                    type="button"
+                    >{{ sub.title }}</Link
+                >
+                <div
+                    v-if="item.badgeNumber"
+                    class="py-1 px-3 rounded flex items-center justify-center text-xs"
+                >
+                    {{ sub.badgeNumber }}
+                </div>
+            </li>
+        </template>
+    </div>
 </template>
